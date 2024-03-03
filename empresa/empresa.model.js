6ef9bf82188d3a9be7599c0724b309caf48bda8c
smaeda2019256 <smaeda-2019256@Kinal.edu.gt>
2024-03-03 12:29:04 -0600

    Se crea el modelo de Empresa

--- empresa/empresa.model.js
+++ empresa/empresa.model.js
@@ -1,1 +1,31 @@
 import mongoose from "mongoose";
+
+const EmpresaSchema = mongoose.Schema({
+    nameEmpresa: {
+        type: String,
+        required: [true, 'The company name is required'],
+        uniqued: true
+    },
+    impactLevel: {
+        type: String,
+        required: [true, 'The company Impact level is required'],
+    },
+    yearsOfTrajectory: {
+        type: String,
+        required: [true, 'The company years of trajectory is required'],
+    },
+    category: {
+        type: String,
+        required: [true, 'The company category is required'],
+    },
+    email: {
+        type: String,
+        required: [true, 'The company email is required'],
+    },
+    contactPhone: {
+        type: String,
+        required: [true, 'The company  phone number is required'],
+    }
+});
+
+export default mongoose.model('Empresa', EmpresaSchema);

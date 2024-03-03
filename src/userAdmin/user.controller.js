b35ab04f5494d0cfa0ff08853f58a1a068487769
smaeda2019256 <smaeda-2019256@Kinal.edu.gt>
2024-03-02 22:18:38 -0600

    Se crea el get de usuarios admin

--- src/userAdmin/user.controller.js
+++ src/userAdmin/user.controller.js
@@ -32,3 +32,20 @@ export const userPost = async (req, res) => {
     }
     
 };
+
+export const usersGet = async(req = request, res = response) => {
+    const { limite, desde } = req.query;
+    const query = { estado: true };
+    const [total, usuarios] = await Promise.all([
+        User.countDocuments(query),
+        User.find(query)
+        .skip(Number(desde))
+        .limit(Number(limite))
+    ]);
+
+    res.status(200).json({
+        msg: 'Admin users added',
+        total,
+        usuarios,
+    });
+}

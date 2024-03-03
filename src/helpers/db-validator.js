168d37542a58bcfc2a253209dcabacdcb4ae1122
smaeda2019256 <smaeda-2019256@Kinal.edu.gt>
2024-03-02 22:30:00 -0600

    Se crea la validacion para ver si el email ya existe

--- src/helpers/db-validator.js
+++ src/helpers/db-validator.js
@@ -1,2 +1,10 @@
 import User from '../userAdmin/user.model.js';
 
+ export const existeEmail = async (email = '') => {
+    const existingEmail = await User.findOne({email});
+    if (existingEmail){
+        throw new Error(`The email ${email} does EXIST in the database`);
+    }
+ }
+
+ 

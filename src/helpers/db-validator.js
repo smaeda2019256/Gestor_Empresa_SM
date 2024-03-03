8e304c1fde01b0b0129664a98c10ffc1a2e1c711
smaeda2019256 <smaeda-2019256@Kinal.edu.gt>
2024-03-02 22:33:09 -0600

    Se crea la validacion para ver si el nombre de usuario ya existe

--- src/helpers/db-validator.js
+++ src/helpers/db-validator.js
@@ -3,8 +3,14 @@ import User from '../userAdmin/user.model.js';
  export const existeEmail = async (email = '') => {
     const existingEmail = await User.findOne({email});
     if (existingEmail){
-        throw new Error(`The email ${email} does EXIST in the database`);
+        throw new Error (`The email ${email} does EXIST in the Database`);
     }
- }
+}
 
- 
+export const existeUsername = async (username = '') => {
+    const existingUsername = await User.findOne({username})
+    if(existingUsername) {
+        throw new Error (`The username ${username} does EXIST in the Database`);
+    }
+}
+

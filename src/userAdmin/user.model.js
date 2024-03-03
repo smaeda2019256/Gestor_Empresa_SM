6ca548f53f49fe5f8f762892d1756354710344d7
smaeda2019256 <smaeda-2019256@Kinal.edu.gt>
2024-03-02 21:34:51 -0600

    Se crean los atributos para el modelo usuario

--- src/userAdmin/user.model.js
+++ src/userAdmin/user.model.js
@@ -1,1 +1,33 @@
-import mongoose from "mongoose";
+import mongoose, { mongo } from "mongoose";
+
+const UserAdminSchema = mongoose.Schema({
+    username: {
+        type: String,
+        required: [true, 'The ADMIN username is required'],
+        uniqued: true
+    },
+    name: {
+        type: String,
+        required: [true, 'The name is required'],
+    },
+    email: {
+        type: String,
+        required: [true, 'The ADMIN email is required'],
+        uniqued: true
+    },
+    password: {
+        type: String,
+        required: [true, 'The password is required'],
+    },
+    role: {
+        type: String,
+        default: 'ADMIN_ROLE',
+        required: [true, 'The role is required'],
+    },
+    state: {
+        type: Boolean,
+        default: true
+    }
+});
+
+export default mongoose.model('Admin', UserAdminSchema);

b976d85a3165a79a69d38f0e6667d8d50a1f61cc
smaeda2019256 <smaeda-2019256@Kinal.edu.gt>
2024-03-03 13:24:04 -0600

    Se crea el validar de token

--- empresa/empresa.routes.js
+++ empresa/empresa.routes.js
@@ -1,3 +1,4 @@
 import { Router } from "express";
 import { check } from "express-validator";
 import { empresaPost, empresaPut } from "./empresa.controller";
+import { existeNameCompany, existingById } from "../src/helpers/db-validator.js";

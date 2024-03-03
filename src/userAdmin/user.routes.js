aa57cfae601531a85e734f0d1bffa4c881c06b05
smaeda2019256 <smaeda-2019256@Kinal.edu.gt>
2024-03-02 23:08:59 -0600

    Se crea la ruta para el post y get de usuario admin

--- src/userAdmin/user.routes.js
+++ src/userAdmin/user.routes.js
@@ -1,4 +1,25 @@
 import { Router } from "express";
 import { check } from "express-validator";
-import { userPost } from "./user.controller.js";
+import { userPost, usersGet } from "./user.controller.js";
+import { validarCampos } from "../middlewares/validarCampos.js";
 import { existeEmail, existeUsername } from "../helpers/db-validator.js";
+
+const router = Router();
+
+router.get("/", usersGet);
+
+router.post(
+    "/",
+    [
+        check('username', 'The username is required').not().isEmpty(),
+        check('username').custom(existeUsername),
+        check('name', 'The name is required').not().isEmpty(),
+        check('email', 'The email is required').not().isEmpty(),
+        check("email", "The email is not valid").isEmail(),
+        check('email').custom(existeEmail),
+        check('password', 'The password must be greater than 6 characters').isLength({min: 6}),
+        validarCampos
+    ], userPost
+)
+
+export default router;

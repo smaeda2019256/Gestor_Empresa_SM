80e62c0c748f099db91d8250cd1231cf95baf3c9
smaeda2019256 <smaeda-2019256@Kinal.edu.gt>
2024-03-02 23:13:41 -0600

    Se crean rutas en el server

--- configs/server.js
+++ configs/server.js
@@ -5,14 +5,15 @@ import cors from 'cors';
 import helmet from 'helmet';
 import morgan from 'morgan'; 
 import { dbConnection } from "./mongo.js";
+import userRoutes from '../src/userAdmin/user.routes.js';
 
 class Server {
     constructor(){
         this.app = express();
         this.port = process.env.PORT;
+        this.userPath = '/coperex/v1/users';
 
         this.conectarDB();
-
         this.middlewares();
         this.routes();
     }
@@ -30,7 +31,7 @@ class Server {
     }
 
     routes(){
-        
+        this.app.use(this.userPath, userRoutes);
     }
 
     listen(){

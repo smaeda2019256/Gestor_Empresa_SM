2b1e5c09d9d38b4675bebed0586c82e80237fcfc
smaeda2019256 <smaeda-2019256@Kinal.edu.gt>
2024-03-03 12:48:49 -0600

    Se crea el POST de Empresa

--- empresa/empresa.controller.js
+++ empresa/empresa.controller.js
@@ -1,3 +1,28 @@
 import {response, request} from 'express';
 import Empresa from './empresa.model.js';
 
+export const empresaPost  = async (req = request, res = response) => {
+
+    try{
+        const {nameEmpresa, impactLevel, yearsOfTrajectory, category, email, contactPhone} = req.body;
+        const company = new Empresa({
+            nameEmpresa,
+            impactLevel,
+            yearsOfTrajectory,
+            category,
+            email, 
+            contactPhone
+        });
+
+        await company.save();
+
+        res.status(200).json({
+            msg: 'Company CREATED successfully',
+            company
+        })
+
+    }catch(error){
+        console.error('ERROR - Creating Company: ', error);
+        return res.status(500).json({error: 'Internal server ERROR'});
+    }
+}
